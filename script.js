f35553e77ba93947e9b1f010e42e26298b72e5b3
agroitec <aagroitec@gmail.com>
2024-06-19 16:34:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function hideInputs() {
+    // document.getElementById('hideButton').style.display = 'none';
+    document.getElementById('videoIframe').style.display = 'block';
+    document.getElementById('videoIframe').scrollIntoView({ behavior: 'smooth' });
+}
